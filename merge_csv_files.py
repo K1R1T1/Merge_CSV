749a44427dd0bf72f3a9ebb25e5695711fbe3daa
Kiriti Yelamanchali <34339284+K1R1T1@users.noreply.github.com>
2017-12-21 10:39:35 +0800

    Python Code upload

---
+++ merge_csv_files.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Author : Kiriti Yelamanchali 
+Title: Merge CSV files into one. 
+"""
+
+###############################################################################
+#Import Libraries 
+import glob
+###############################################################################
+
+###############################################################################
+#User Input 
+#Change the input directory
+directory = "C:/Users/ABCD/Desktop/Test" 
+###############################################################################
+
+
+###############################################################################
+#Code
+to_merge = glob.glob(directory+"/*.csv")
+to_merge.sort()
+header_saved = False
+outputfile = "Merged_File.csv"
+with open(outputfile,'w', encoding = 'utf-8-sig') as mergedfile:
+    for filename in to_merge:
+        with open(filename, encoding = 'utf-8-sig') as infiles:
+            header = next(infiles)
+            if not header_saved:
+                mergedfile.write(header)
+                header_saved = True
+            for line in infiles:
+                mergedfile.write(line)
+print("Check the merged file located at: "+ directory + "/" + outputfile)
+###############################################################################
